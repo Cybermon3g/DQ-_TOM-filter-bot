c9e0809f4600346b19dd4f2fc1414131bacd3c30
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-18 23:56:59 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1533,19 +1533,17 @@ async def cb_handler(client: Client, query: CallbackQuery):
 
 
 
-    elif query.data.startswith("close_data"):
-        userid = query.message.reply_to_message.from_user.id
-        if int(query.from_user.id) not in [query.from_user.id, 0]:
-            await query.answer("𝐂𝐥𝐨𝐬𝐞𝐝", show_alert=True)                     
-            await query.message.delete()
-            await query.message.reply_to_message.delete()            
-        else:
-            await query.answer("𝐂𝐥𝐨𝐬𝐞𝐝", show_alert=True)
-            await query.message.delete()
-            await query.message.reply_to_message.delete()
-#        return await query.answer("𝐂𝐥𝐨𝐬𝐞𝐝", show_alert=True) 
-
-
+#    elif query.data.startswith("close_data"):
+#        userid = query.message.reply_to_message.from_user.id
+#        if int(query.from_user.id) not in [query.from_user.id, 0]:
+#            await query.answer("𝐂𝐥𝐨𝐬𝐞𝐝", show_alert=True)                     
+#            await query.message.delete()
+#            await query.message.reply_to_message.delete()            
+#        else:
+#            await query.answer("𝐂𝐥𝐨𝐬𝐞𝐝", show_alert=True)
+#            await query.message.delete()
+#            await query.message.reply_to_message.delete()
+#       
 
 
 
