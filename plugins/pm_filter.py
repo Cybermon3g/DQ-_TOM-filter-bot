f5a81a4f1c3211fd6b10e4da10aff2dece55d18f
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-18 19:29:09 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1249,6 +1249,10 @@ async def cb_handler(client: Client, query: CallbackQuery):
             )
 
     elif query.data.startswith("abc"):
+        query = re.sub(
+            r"\b(pl(i|e)*?(s|z+|ease|se|ese|(e+)s(e)?)|((send|snd|giv(e)?|gib)(\sme)?)|movie(s)?|new|latest|br((o|u)h?)*|^h(e|a)?(l)*(o)*|mal(ayalam)?|t(h)?amil|file|that|find|und(o)*|kit(t(i|y)?)?o(w)?|thar(u)?(o)*w?|kittum(o)*|aya(k)*(um(o)*)?|full\smovie|any(one)|with\ssubtitle(s)?)",
+            "", query.message.text, flags=re.IGNORECASE)  # plis contribute some common words
+        query = query.strip() + " movie"
         content = query.message.text
         im = await get_poster(content) if IMDB else None
         await query.answer(f"{im.get('title')}", show_alert=True)
