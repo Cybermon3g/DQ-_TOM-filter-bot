80c4de08df01938172fd408bcb3a41c42795a43c
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-20 14:41:07 +0530

    Update Chat.py

--- plugins/private/Chat.py
+++ plugins/private/Chat.py
@@ -3,7 +3,7 @@ import os
 import logging
 from pyrogram import Client, filters, enums
 from Script import script
-from info import CHANNELS, ADMIN, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, LOG_CHANNEL, PM, ADMINS
+from info import CHANNELS, ADMIN, AUTH_CHANNEL, CUSTOM_FILE_CAPTION, LOG_CHANNEL, PM
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 
 
@@ -11,7 +11,7 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-@Client.on_message(filters.private & filters.text)
+@Client.on_message(filters.command("chat") & filters.text)
 async def pm_text(client: Client, message):
     try:
         if message.from_user.id == ADMIN:
@@ -21,7 +21,7 @@ async def pm_text(client: Client, message):
         reference_id = int(message.chat.id)
         k = await client.send_message(
             chat_id=ADMIN,
-            text=script.PM_TXT_ATT.format(reference_id, info.first_name),
+            text=script.PM_TXT_ATT.format(reference_id, info.first_name, message.text),
             parse_mode=enums.ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
                         [
@@ -39,7 +39,7 @@ async def pm_text(client: Client, message):
         await client.send_message(
             chat_id=PM,
             text=script.PM_TXT_ATT.format(reference_id, info.first_name, message.text),
-            parse_mode="html",
+            parse_mode=enums.ParseMode.HTML,
             reply_markup=InlineKeyboardMarkup(
                         [
                             [
@@ -59,20 +59,20 @@ async def pm_text(client: Client, message):
         logger.exception(e)
 
 
-@Client.on_message(filters.private & filters.media)
-async def pm_media(bot, message):
-    if message.from_user.id in ADMINS:
-        await replay_media(bot, message)
-        return
-    info = await bot.get_users(user_ids=message.from_user.id)
-    reference_id = int(message.chat.id)
-    await bot.copy_message(
-        chat_id=ADMINS,
-        from_chat_id=message.chat.id,
-        message_id=message.message_id,
-       caption=script.PM_MED_ATT.format(reference_id, info.first_name),
-        parse_mode=enums.ParseMode.HTML
-    )
+# @Client.on_message(filters.private & filters.media)
+# async def pm_media(bot, message):
+#     if message.from_user.id in ADMINS:
+#         await replay_media(bot, message)
+#         return
+#     info = await bot.get_users(user_ids=message.from_user.id)
+#     reference_id = int(message.chat.id)
+#     await bot.copy_message(
+#         chat_id=ADMINS,
+#         from_chat_id=message.chat.id,
+#         message_id=message.message_id,
+#         caption=script.PM_MED_ATT.format(reference_id, info.first_name),
+#         parse_mode="html"
+#     )
 
 
 @Client.on_message(filters.private & filters.user(ADMIN) & filters.text & filters.reply)
