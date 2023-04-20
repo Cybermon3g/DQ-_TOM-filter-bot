ad073ce583e2744a5e3b435151db56dd29bf4cdf
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-20 16:23:05 +0530

    Update Chat.py

--- plugins/private/Chat.py
+++ plugins/private/Chat.py
@@ -64,11 +64,11 @@ async def pm_text(client: Client, message):
 @Client.on_message(filters.private & filters.media)
 async def pm_media(bot, message):
     if message.from_user.id in ADMINS:
-        await replay_media(bot, message)
+        await reply_media(bot, message)
         return
     info = await bot.get_users(user_ids=message.from_user.id)
     reference_id = int(message.chat.id)
-    await bot.copy_message(
+    await message.copy(
         chat_id=ADMINS,
         from_chat_id=message.chat.id,
         message_id=message.id,
@@ -129,7 +129,7 @@ async def replay_media(client: Client, message):
                 reference_id = file.caption.split()[2]
             except Exception:
                 pass
-            await client.copy_message(
+            await message.copy(
                 chat_id=int(reference_id),
                 from_chat_id=message.chat.id,
                 message_id=message.id,
