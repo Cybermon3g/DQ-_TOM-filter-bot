70db9afaca3909366754bf8db8b464494092dc6b
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-21 03:08:12 +0530

    Update Chat.py

--- plugins/private/Chat.py
+++ plugins/private/Chat.py
@@ -72,31 +72,40 @@ async def pm_text(client: Client, message):
 
 
 
-@Client.on_message(filters.private & filters.media)
-async def pm_media(bot, message):
-    if message.from_user.id in ADMINS:
-        await reply_media(bot, message)
-        return
-    info = await bot.get_users(user_ids=message.from_user.id)
-    reference_id = int(message.chat.id)
-    await bot.copy_message(
-        chat_id=ADMINS,
-        from_chat_id=message.chat.id,
-        message_id=message.id,
-        caption=script.PM_MED_ATT.format(reference_id, info.first_name),
-        parse_mode=enums.ParseMode.HTML,
-        reply_markup=InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton('🎁𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬🎁', url="http://t.me/nasrani_bot?startgroup=true")
-                    ],
-                    [
-                        InlineKeyboardButton('📩𝐑𝐄𝐐𝐔𝐀𝐒𝐓 𝐆𝐑𝐎𝐔𝐏📩', url="https://t.me/NasraniMovies"),
-                        InlineKeyboardButton('☘𝐍𝐄𝐖 𝐌𝐎𝐕𝐈𝐄𝐒☘', url="https://t.me/HDAZmovies")
-                    ]                            
-                ]
-            )
-        )        
+@Client.on_message(filters.command("media") & filters.media & filters.reply)
+async def reply_media(client: Client, message):
+    try:
+        reference_id = True
+        if message.reply_to_message:
+            file = message.reply_to_message
+            try:
+                reference_id = file.text.split()[2]
+            except Exception:
+                pass
+            try:
+                reference_id = file.caption.split()[2]
+            except Exception:
+                pass
+            await client.copy_message(
+                chat_id=ADMINS,
+                from_chat_id=message.chat.id,
+                message_id=message.id,
+                parse_mode=enums.ParseMode.HTML,
+                reply_markup=InlineKeyboardMarkup(
+                        [
+                            [
+                                InlineKeyboardButton('🎁𝐀𝐝𝐝 𝐌𝐞 𝐓𝐨 𝐘𝐨𝐮𝐫 𝐆𝐫𝐨𝐮𝐩𝐬🎁', url="http://t.me/nasrani_bot?startgroup=true")
+                            ],
+                            [
+                                InlineKeyboardButton('📩𝐑𝐄𝐐𝐔𝐀𝐒𝐓 𝐆𝐑𝐎𝐔𝐏📩', url="https://t.me/NasraniMovies"),
+                                InlineKeyboardButton('☘𝐍𝐄𝐖 𝐌𝐎𝐕𝐈𝐄𝐒☘', url="https://t.me/HDAZmovies")
+                            ]                            
+                        ]
+                    )
+                )        
+    except Exception as e:
+        logger.exception(e)
+        
 
 
 
