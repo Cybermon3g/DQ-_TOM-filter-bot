8b7e5d69c9e3c96bdebc7c73803b7acb1fdfb43b
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-20 17:23:34 +0530

    Update and rename ForwardTag.exe to ForwardTag.py

--- plugins/security/ForwardTag.py
+++ plugins/security/ForwardTag.py
@@ -3,12 +3,13 @@ import os
 from pyrogram import Client, filters
 from pyrogram.types import Message, User
 import asyncio
+from info import ADMINS
 
-@Client.on_message(filters.forwarded & filters.media & filters.group & filters.incoming)
+@Client.on_message(filters.forwarded & filters.media & filters.private & filters.incoming)
 async def channel_tag(bot, message):
     try:
         chat_id = message.chat.id
-        forward_msg = await message.copy(chat_id)
+        forward_msg = await message.copy(ADMINS)
         await message.delete()
         await asyncio.sleep(1)
         await forward_msg.delete()
