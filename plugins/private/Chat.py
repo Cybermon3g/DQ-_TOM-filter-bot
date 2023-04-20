bcb78f0f9a9da2ed4e58a32db71fd234eac8e49f
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-21 01:30:38 +0530

    Update Chat.py

--- plugins/private/Chat.py
+++ plugins/private/Chat.py
@@ -71,7 +71,7 @@ async def pm_media(bot, message):
     await bot.copy_message(
         chat_id=ADMINS,
         from_chat_id=message.chat.id,
-        message_id=media.id,
+        message_id=message.id,
         caption=script.PM_MED_ATT.format(reference_id, info.first_name),
         parse_mode=enums.ParseMode.HTML
     )
@@ -132,7 +132,7 @@ async def replay_media(client: Client, message):
             await client.copy_message(
                 chat_id=int(reference_id),
                 from_chat_id=message.chat.id,
-                message_id=media.id,
+                message_id=message.id,
                 parse_mode=enums.ParseMode.HTML,
                 reply_markup=InlineKeyboardMarkup(
                         [
