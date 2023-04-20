06da2b8b48dfa0e3b758da1732083f26122cdf10
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-20 16:11:57 +0530

    Update Chat.py

--- plugins/private/Chat.py
+++ plugins/private/Chat.py
@@ -63,20 +63,20 @@ async def pm_text(client: Client, message):
 
 @Client.on_message(filters.private & filters.media)
 async def pm_media(bot, message):
-    reply = message.reply_to_message
-    reply_id = message.reply_to_message.id if message.reply_to_message else message.id
     if message.from_user.id in ADMINS:
-        await reply_media(bot, message)
+        await replay_media(bot, message)
         return
     info = await bot.get_users(user_ids=message.from_user.id)
     reference_id = int(message.chat.id)
-    await message.copy(
+    await bot.copy_message(
         chat_id=ADMINS,
         from_chat_id=message.chat.id,
-        reply_to_message_id=reply_id,
+        message_id=message.message_id,
         caption=script.PM_MED_ATT.format(reference_id, info.first_name),
-        parse_mode=enums.ParseMode.HTML,
+        parse_mode="html"
     )
+
+
 
     
 
