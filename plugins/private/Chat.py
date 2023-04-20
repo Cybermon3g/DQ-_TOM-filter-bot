6999e9e630c87306360e93f86d3efce64fe253ba
azanhelpdesk <128210477+azanhelpdesk@users.noreply.github.com>
2023-04-21 03:57:14 +0530

    Update Chat.py

--- plugins/private/Chat.py
+++ plugins/private/Chat.py
@@ -63,10 +63,7 @@ async def pm_text(client: Client, message):
         await asyncio.sleep(3000)
         await k.delete()
 
-    except Exception as e:
-        logger.exception(e)    
-
-
+    
 
 
 
